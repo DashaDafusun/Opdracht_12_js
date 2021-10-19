3200c99ea0aed1a7e46439ea35574d57e4608e04
DashaDafusun <84176953+DashaDafusun@users.noreply.github.com>
2021-10-19 12:56:51 +0200

    Add files via upload

---
+++ opdracht_12_js/script.js
@@ -0,0 +1,11 @@
+
+console.log('Hallo Winc Academy Studenten!');
+
+// let a = 1230941;
+// let b = 1823794;
+//Dit is een grote som
+// console.log(a * b);
+
+// let c = 637263;
+// let d = 54;
+// console.log(c / d);
